d8e68209b09bdc0218772bc1f745ce1a6e2cbede
eliasylee <eliasylee@gmail.com>
2016-10-10 19:37:57 -0700

    added frontend folder with naive files

--- frontend/the_line_game.jsx
+++ frontend/the_line_game.jsx
@@ -1,3 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import Root from './components/root';
+
 document.addEventListener('DOMContentLoaded', () => {
-  
+  let store;
+
+  window.store = store;
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={store} />, root);
 });
