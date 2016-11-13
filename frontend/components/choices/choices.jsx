38123f677ea9d37e80f652598a8a9a24ba217ff9
eliasylee <eliasylee@gmail.com>
2016-11-12 18:09:03 -0800

    added clear all choices button

--- frontend/components/choices/choices.jsx
+++ frontend/components/choices/choices.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import ChoicesItem from './choices_item';
 import ChoicesFormContainer from '../choices_form/choices_form_container';
+import { withRouter } from 'react-router';
 
 class Choices extends React.Component {
+  constructor() {
+    super();
+    this.handleClearChoices = this.handleClearChoices.bind(this);
+    this.handleBeginFlow = this.handleBeginFlow.bind(this);
+  }
+
+  handleClearChoices() {
+    this.props.clearChoices();
+  }
+
+  handleBeginFlow() {
+    this.props.router.push("/");
+  }
+
   renderChoices() {
     if (this.props && this.props.choices) {
       const { choices } = this.props;
@@ -26,6 +41,16 @@ class Choices extends React.Component {
       <div className="choicesPage">
         <div className="choicesHeader">The Line Game</div>
         <ChoicesFormContainer action="Create"/>
+        <div className="choicesActionBar">
+          <div className="clearChoicesBox"
+               onClick={this.handleClearChoices}>
+               Clear All
+          </div>
+          <div className="Begin Flow"
+               onClick={this.handleBeginFlow}>
+               Begin Flow
+          </div>
+        </div>
         <div className="choicesList">
           <div className="choicesListHeader">
             <div>Choices</div>
@@ -38,4 +63,4 @@ class Choices extends React.Component {
   }
 }
 
-export default Choices;
+export default withRouter(Choices);
